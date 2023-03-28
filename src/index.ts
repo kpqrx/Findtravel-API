44fe82ca440005b43b21879428a311c75af8e252
Kamil Pyszkowski <kamilpyszkowski@gmail.com>
2023-03-28 16:28:43 +0200

    Installed dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const { PORT } = process.env
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(3000, () => console.log('🚀 Listening on port 3000...'))
+app.listen(PORT, () => console.log(`🚀 Listening on port ${PORT}...`))
