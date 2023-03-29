7ce9bb8f13b43d97e2b49844cd090a19b3da4bbb
Kamil Pyszkowski <kamilpyszkowski@gmail.com>
2023-03-29 02:59:39 +0200

    Added file structure, installed paths resolver

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Router } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
@@ -6,7 +6,10 @@ dotenv.config()
 const { PORT } = process.env
 
 const app = express()
+const router = Router()
+
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
 
 app.listen(PORT, () => console.log(`🚀 Listening on port ${PORT}...`))
